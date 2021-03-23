d2b96bec5e3ef0caa0dc5d5b7d9c320622d3b438
Colin Tseng <klt.aus@yahoo.com>
2021-03-23 20:21:22 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+
+let newHeader = document.createElement("h1")
+
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "James is the champion"
+
+
